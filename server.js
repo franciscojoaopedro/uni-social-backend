aee0ee0e7cee9aaa7a6961e11f466666670cb30b
franciscojoaopedro <franciscojoaopedro1998@gmail.com>
2022-10-22 07:01:55 +0100

    backend testes

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const app=express();
 const porta=process.env.PORTA || 3000;
 
 app.get("https://uni-social.onrender.com/",(req,res)=>{
-    res.json({error:false,message:"Hello World!"})
+    res.json({error:false,message:"Hello World!"});
+    res.send({error:false,message:"Hello World!"});
 });
 
 app.listen(porta,()=>{
