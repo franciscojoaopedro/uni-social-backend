d0c41d8a5577d2fef63360380b4de0ce2e6eae79
Walany Costa <walanyBnegro@gmail.com>
2022-11-17 15:53:11 +0000

    add eslint e prettier

--- src/app.js
+++ src/app.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
-
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 app.routes(routes);
-
 app.listen(process.env.PORTA || 3333);
